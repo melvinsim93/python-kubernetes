c9da73ff11accd7f0dd6d34a1d23bc0d3cac125a
anilbidari <anil.bidari@thecloudenabled.com>
2025-02-15 16:27:08 +0530

    Create kubeadm.sh

---
+++ kubeadm.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Update and install necessary packages
+sudo apt update
+sudo apt install -y openjdk-17-jdk  vim git tree maven unzip zip
+
+# Enable password authentication for SSH
+sudo sed -i '58iPasswordAuthentication yes' /etc/ssh/sshd_config
+sudo sed -i '59iChallengeResponseAuthentication yes' /etc/ssh/sshd_config
+
+# Restart SSH service to apply changes
+sudo systemctl restart sshd
+
+# Create a new user (ubuntu) without a password and set password manually
+sudo adduser ubuntu --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password 
+echo "ubuntu:CloudEnabled" | sudo chpasswd
+
+# Redirect output to a log file
+exec > /var/log/user-data.log 2>&1
+
+# Fetch EC2 instance's internal FQDN
+HOSTNAME_FQDN=$(curl -s http://169.254.169.254/latest/meta-data/local-hostname)
+
+# Disable swap
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+# Load kernel modules
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Set sysctl parameters
+cat <<EOT | sudo tee /etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOT
+
+sudo sysctl --system
+
+# Install dependencies
+sudo apt update
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+
+# Add Docker repository and install containerd
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y containerd.io
+
+# Configure containerd
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+
+# Restart and enable containerd
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+# Add Kubernetes repository
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Install Kubernetes components
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
