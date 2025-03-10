de175e79ecfc49f2c4bc1b9ecaf3d25b0ec25f0f
Melvin Sim <96096344+melvinsim93@users.noreply.github.com>
2025-03-10 14:44:22 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello I Melvin, this is Version 1.1!\n"
+    return "Hello I am Melvin, this is Version 3!\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
