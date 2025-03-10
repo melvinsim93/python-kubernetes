c80f2544593b513228f0de74a736dd8e84c52d50
Melvin Sim <96096344+melvinsim93@users.noreply.github.com>
2025-03-10 14:20:13 +0800

    Update kubeadm.sh

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello I Melvin, this is Version 1.1!\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
